9918442833b9986f6f41a78ccc94bdcb246b3cf1
SatoYusuke0228 <62002722+SatoYusuke0228@users.noreply.github.com>
2020-09-10 18:03:30 +0900

    エントリーポイントにextends SpringBootServletInitializerの記述を追加

--- src/main/java/net/SpringBootWebAppApplication.java
+++ src/main/java/net/SpringBootWebAppApplication.java
@@ -2,13 +2,14 @@ package net;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * エントリーポイント
  * @author SatoYusuke0228
  */
 @SpringBootApplication
-public class SpringBootWebAppApplication {
+public class SpringBootWebAppApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebAppApplication.class, args);
