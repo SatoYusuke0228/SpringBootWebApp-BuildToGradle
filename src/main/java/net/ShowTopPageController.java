e769608da870e3cfbe5e55cc8f59cc2b324926b3
SatoYusuke0228 <62002722+SatoYusuke0228@users.noreply.github.com>
2020-09-10 18:34:38 +0900

    デプロイしたWEBのTOPページを表示できない為、@RequestMapping("/")のTOPページを表示するControllerのみ上位のパッケージに移動

---
+++ src/main/java/net/ShowTopPageController.java
@@ -0,0 +1,99 @@
+package net;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import net.product.MsProductCategoryInventoryService;
+import net.product.TrProductEntity;
+
+@Controller
+public class ShowTopPageController {
+
+	@Autowired
+	MsProductCategoryInventoryService categoryService;
+
+	/**
+	 * TOPページでオススメ商品を「productCategoryId == 0」から４種取得するためのメソッド。
+	 *
+	 * 「商品の表示設定がON」になっている商品の数を算定し、
+	 * その数が４以上の場合のみ、４種類のオススメ商品をピックアップして、
+	 * TOPページのスライド写真として掲載する。
+	 *
+	 * @author SatoYusuke0228
+	 */
+	@RequestMapping("/index")
+	public ModelAndView showRecommendedItems(ModelAndView mav) {
+
+		//カテゴリーIDが0の商品のみを取得
+		List<TrProductEntity> items = categoryService.findById(0).get().getTrProductEntity();
+
+		//「商品の表示設定がON」になっている商品の数を示す変数
+		int showFlag = 0;
+
+		//「商品の表示設定がON」になっている商品の数を算定
+		for (TrProductEntity check : items) {
+			if (check.getProductShowFlag() == 0) {
+				showFlag++;
+			}
+		}
+
+		if (4 <= showFlag) {
+			mav.addObject("recommendedItems", randomPickupRecommendedItems(items));
+		}
+
+		//取得した全販売商品データをランダムで商品ピックアップするメソッドに渡してmodelに保存
+		mav.setViewName("index");
+
+		return mav;
+	}
+
+	/**
+	 * TOPページで商品情報を取得するためのヘルパー関数。
+	 * {productCategryId == 0}に属する商品Listを受け取り、
+	 * ランダムで４種オススメ商品をピックアップして格納したListを戻り値として返す。
+	 *
+	 * @param items
+	 * 引数。{productCategryId == 0}に属する全商品が格納されているList
+	 *
+	 * @author SatoYusuke0228
+	 */
+	private List<TrProductEntity> randomPickupRecommendedItems(List<TrProductEntity> items) {
+
+		//オススメの商品リストのインスタンスを作成
+		List<TrProductEntity> recommendedItems = new ArrayList<TrProductEntity>();
+
+		//ランダムクラスのインスタンスを作成
+		Random random = new Random();
+
+		//オススメ商品Listのサイズが４つになるまで商品をランダム抽選
+		while (recommendedItems.size() < 4) {
+
+			//ランダムに抽出した商品と、すでにオススメ商品Listに含まれる商品の重複数
+			int duplication = 0;
+
+			//商品をランダム抽選
+			int randomIndex = random.nextInt(items.size() - 1);
+			TrProductEntity randomPickupItem = items.get(randomIndex);
+
+			//ランダムに抽出した商品と、すでにオススメ商品Listに含まれる商品が重複していないかチェック
+			for (int i = recommendedItems.size(); 0 < i; i--) {
+				if (randomPickupItem == recommendedItems.get(i - 1)) {
+					duplication++;
+				}
+			}
+
+			//重複していない場合のみ、オススメ商品Listに格納する
+			if (duplication == 0 && randomPickupItem.getProductShowFlag() == 0) {
+				recommendedItems.add(randomPickupItem);
+			}
+		}
+
+		return recommendedItems;
+	}
+}
--- src/main/java/net/product/ShowItemsController.java
+++ src/main/java/net/product/ShowItemsController.java
@@ -1,8 +1,6 @@
 package net.product;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -25,85 +23,6 @@ public class ShowItemsController {
 	//セッションスコープのインスタンス
 	//	@Autowired
 	//	private HttpSession session;
-
-	/**
-	 * TOPページでオススメ商品を「productCategoryId == 0」から４種取得するためのメソッド。
-	 *
-	 * 「商品の表示設定がON」になっている商品の数を算定し、
-	 * その数が４以上の場合のみ、４種類のオススメ商品をピックアップして、
-	 * TOPページのスライド写真として掲載する。
-	 *
-	 * @author SatoYusuke0228
-	 */
-	@RequestMapping("/")
-	public ModelAndView showRecommendedItems(ModelAndView mav) {
-
-		//カテゴリーIDが0の商品のみを取得
-		List<TrProductEntity> items = categoryService.findById(0).get().getTrProductEntity();
-
-		//「商品の表示設定がON」になっている商品の数を示す変数
-		int showFlag = 0;
-
-		//「商品の表示設定がON」になっている商品の数を算定
-		for (TrProductEntity check : items) {
-			if (check.getProductShowFlag() == 0) {
-				showFlag++;
-			}
-		}
-
-		if (4 <= showFlag) {
-			mav.addObject("recommendedItems", randomPickupRecommendedItems(items));
-		}
-
-		//取得した全販売商品データをランダムで商品ピックアップするメソッドに渡してmodelに保存
-		mav.setViewName("index");
-
-		return mav;
-	}
-
-	/**
-	 * TOPページで商品情報を取得するためのヘルパー関数。
-	 * {productCategryId == 0}に属する商品Listを受け取り、
-	 * ランダムで４種オススメ商品をピックアップして格納したListを戻り値として返す。
-	 *
-	 * @param items
-	 * 引数。{productCategryId == 0}に属する全商品が格納されているList
-	 *
-	 * @author SatoYusuke0228
-	 */
-	private List<TrProductEntity> randomPickupRecommendedItems(List<TrProductEntity> items) {
-
-		//オススメの商品リストのインスタンスを作成
-		List<TrProductEntity> recommendedItems = new ArrayList<TrProductEntity>();
-
-		//ランダムクラスのインスタンスを作成
-		Random random = new Random();
-
-		//オススメ商品Listのサイズが４つになるまで商品をランダム抽選
-		while (recommendedItems.size() < 4) {
-
-			//ランダムに抽出した商品と、すでにオススメ商品Listに含まれる商品の重複数
-			int duplication = 0;
-
-			//商品をランダム抽選
-			int randomIndex = random.nextInt(items.size() - 1);
-			TrProductEntity randomPickupItem = items.get(randomIndex);
-
-			//ランダムに抽出した商品と、すでにオススメ商品Listに含まれる商品が重複していないかチェック
-			for (int i = recommendedItems.size(); 0 < i; i--) {
-				if (randomPickupItem == recommendedItems.get(i - 1)) {
-					duplication++;
-				}
-			}
-
-			//重複していない場合のみ、オススメ商品Listに格納する
-			if (duplication == 0 && randomPickupItem.getProductShowFlag() == 0) {
-				recommendedItems.add(randomPickupItem);
-			}
-		}
-
-		return recommendedItems;
-	}
 
 	/**
 	 * 商品一覧ページをカテゴリーごとに表示するためのメソッド
