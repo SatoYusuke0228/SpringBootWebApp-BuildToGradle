d77879e849e17604e11860e633361c6cdb2f045d
SatoYusuke0228 <62002722+SatoYusuke0228@users.noreply.github.com>
2020-09-10 19:40:21 +0900

    エントリーポイントでconfigureメソッドをoverrideしてみる

--- src/main/java/net/SpringBootWebAppApplication.java
+++ src/main/java/net/SpringBootWebAppApplication.java
@@ -2,6 +2,7 @@ package net;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
@@ -14,4 +15,11 @@ public class SpringBootWebAppApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebAppApplication.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebAppApplication.class);
+    }
+
+
 }
